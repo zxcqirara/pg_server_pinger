4823f84fa45c03cf4aba89f5d046998ea9f50b08
zxcqirara <zxcqirara@users.noreply.github.com>
2024-11-21 22:50:25 +0300

    PORT moved to .env var

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,21 @@ use axum::{
 	routing::get,
 	Router,
 };
+use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() {
+	dotenv().ok();
+
 	// build our application with a single route
 	let app = Router::new()
 		.route("/", get(|| async { "Hello, World!" }));
 
 	// run our app with hyper, listening globally on port 3000
-	let listener = tokio::net::TcpListener::bind("0.0.0.0:20042").await.unwrap();
+	let port = std::env::var("PORT")
+		.expect("PORT environment variable not set");
+
+	let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}")).await.unwrap();
 	println!("Listening on {}", listener.local_addr().unwrap());
 	
 	axum::serve(listener, app).await.unwrap();
