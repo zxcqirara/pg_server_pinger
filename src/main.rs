332f14dffdde4144d64af9bd1d23099a51262e96
zxcqirara <zxcqirara@users.noreply.github.com>
2024-11-22 18:08:12 +0300

    Implemented logging

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ use std::env;
 use std::net::{TcpStream};
 use std::time::{Duration, Instant};
 use tokio::time::sleep;
+use tracing::{error, info};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-	dotenv().ok();
-	
+	dotenv().ok();      // Init .env file
+	env_logger::init(); // Init logger
+
+	// Get some vars from env
 	let target_host = env::var("TARGET_HOST")
 		.expect("TARGET_HOST is not set");
 
@@ -22,20 +25,22 @@ async fn main() -> anyhow::Result<()> {
 		.parse()
 		.expect("DELAY_SECS is not a u64 number");
 
-	println!("{}", format!("Checking {target_host}...").blue());
+	info!("{}", format!("Checking {target_host}...").blue());
 
+	// Init timeouts and delays
 	let timeout = Duration::from_secs(10);
 	let delay_secs = Duration::from_secs(delay_secs);
 	let mut is_last_ping_success = false;
 
+	// Main check loop
 	loop {
 		match ping_server(&target_host, target_port, timeout) {
 			Some(delay) if !is_last_ping_success => {
-				println!("{}", format!("Ping successful! Delay: {:0.2?}", delay).green());
+				info!("{}", format!("Success: {:0.2?}", delay).green());
 				is_last_ping_success = true;
 			},
 			None if is_last_ping_success => {
-				println!("{}", "Failed to ping the server".red());
+				error!("{}", "Failed to ping the server".red());
 				is_last_ping_success = false;
 			},
 			_ => {}
@@ -43,10 +48,9 @@ async fn main() -> anyhow::Result<()> {
 
 		sleep(delay_secs).await;
 	}
-
-	Ok(())
 }
 
+// This function is written by ChatGPT lol
 fn ping_server(host: &str, port: u16, timeout: Duration) -> Option<Duration> {
 	let address = format!("{}:{}", host, port);
 	let start = Instant::now();
